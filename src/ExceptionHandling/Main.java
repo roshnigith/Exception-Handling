79a3741c32f205d379e8a06d7151f168caaeb4f7
Roshni <roshni81198singh@gmail.com>
2024-04-14 10:54:30 +0530

    added try catch

--- src/ExceptionHandling/Main.java
+++ src/ExceptionHandling/Main.java
@@ -7,11 +7,23 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number:");
         int n = sc.nextInt();
+        int[] arr = new int[5];
         try{
             int res = 10/n;
+            for(int i = 0;i <= 5;i++){
+                arr[i] = i;
+                System.out.println(arr[i]);
+            }
         }catch(ArithmeticException a){
-            System.out.println("Divide by zero error");
+            a.printStackTrace();
+            System.out.println((a.getStackTrace())); //line no and other details in the exception
+            System.out.println((a.getMessage()));
+        }catch(ArrayIndexOutOfBoundsException e){
+            System.out.println((e.getMessage()));
+        }catch(Exception e){ // Parent Exception
+            System.out.println((e.getMessage()));
         }
         System.out.println("This is an important statement and should be printed");
     }
